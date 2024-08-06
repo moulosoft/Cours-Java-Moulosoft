94e8c61349cb8c393c7577f737630250e217cd56
nkwentyalexander <155551772+nkwentyalexander@users.noreply.github.com>
2024-08-06 18:40:31 +0100

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,7 +5,7 @@ HEAD
 		System.out.println("Hello World, how far");
 
 		System.out.println("Hello World , how are you");
-		System.out.println("hello moulosoft");
+		System.out.println("hello moulosoft///");
 070c2999a545f6b14748bf5c12b2103205d39374
 	}
 }
