bf3a1be146044d935d8c26caa6afabac40cf5092
nkwentyalexander <155551772+nkwentyalexander@users.noreply.github.com>
2024-08-06 18:37:10 +0100

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,9 +2,10 @@ public class HelloWorld {
 	
 	public static void main(String[] args) {
 HEAD
-		System.out.println("Hello World, how far,,,,");
+		System.out.println("Hello World, how far");
 
 		System.out.println("Hello World , how are you");
+		System.out.println("hello moulosoft");
 070c2999a545f6b14748bf5c12b2103205d39374
 	}
 }
