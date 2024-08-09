6e55161c6270d20d290c8611f0f4c7946803f3a0
moulosoft <137833245+moulosoft@users.noreply.github.com>
2024-08-10 00:16:50 +0200

    Update NumberSystems.java

--- src/com/itbulls/learnit/javacore/numbersystems/NumberSystems.java
+++ src/com/itbulls/learnit/javacore/numbersystems/NumberSystems.java
@@ -1,17 +1,10 @@
-package com.itbulls.learnit.javacore.numbersystems;
-
 public class NumberSystems {
-	
-	public static void main(String[] args) {
-		int dec = 152;            //  no prefix   --> decimal literal
-		int bin = 0b10011000;     // '0b' prefix --> binary literal
-		int oct = 0230;           // '0' prefix  --> octal literal
-		int hex = 0x98;           // '0x' prefix --> hexadecimal literal
-		
-		System.out.println(dec);
-		System.out.println(bin);
-		System.out.println(oct);
-		System.out.println(hex);
-	}
-
-}
+    
+    public static void main(String[] args) {
+        int dec = 152; // Déclaration d'un entier décimal (base 10)
+        int bin = 0b10011000; // Déclaration d'un entier binaire (base 2)
+        int oct = 0230; // Déclaration d'un entier octal (base 8)
+        int hex = 0x98; // Déclaration d'un entier hexadécimal (base 16)
+        
+        System.out.println(dec); // Affichage de la valeur décimale
+        Sy
