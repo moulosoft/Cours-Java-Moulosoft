96ba5ced03b97ba6acf0f4f944eee0bf4859f85c
moulosoft <137833245+moulosoft@users.noreply.github.com>
2024-08-28 23:58:50 +0200

    Update VariantProduct.java

--- src/com/itbulls/learnit/javacore/oop/classes/typesofclasses/VariantProduct.java
+++ src/com/itbulls/learnit/javacore/oop/classes/typesofclasses/VariantProduct.java
@@ -1,16 +1,22 @@
 package com.itbulls.learnit.javacore.oop.classes.typesofclasses;
 
+// Indique le package auquel appartient la classe
+
 public class VariantProduct extends Product {
 	
-	private MasterProduct[] variants;
+	// Attribut privé de la classe VariantProduct
+	private MasterProduct[] variants; // Tableau d'objets de type MasterProduct
 
 	@Override
 	public boolean isAvailableInStock() {
+		// Parcourt le tableau de variantes
 		for (MasterProduct masterProduct : variants) {
+			// Vérifie si au moins une variante est disponible en stock
 			if (masterProduct.isAvailableInStock()) {
 				return true;
 			}
 		}
+		// Si aucune variante n'est disponible, retourne false
 		return false;
 	}
 
