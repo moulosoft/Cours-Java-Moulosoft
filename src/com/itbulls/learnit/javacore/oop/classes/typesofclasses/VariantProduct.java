e81c849bdf28c2ad5ee21711c71f3cf1ff188125
moulosoft <137833245+moulosoft@users.noreply.github.com>
2024-08-29 00:04:08 +0200

    Update VariantProduct.java

--- src/com/itbulls/learnit/javacore/oop/classes/typesofclasses/VariantProduct.java
+++ src/com/itbulls/learnit/javacore/oop/classes/typesofclasses/VariantProduct.java
@@ -11,6 +11,10 @@ public class VariantProduct extends Product {
 	public boolean isAvailableInStock() {
 		// Parcourt le tableau de variantes
 		for (MasterProduct masterProduct : variants) {
+/*
+Débute une boucle for qui parcourt chaque élément du tableau variants.
+À chaque itération, la variable masterProduct contient une référence à l'élément courant du tableau.
+*/
 			// Vérifie si au moins une variante est disponible en stock
 			if (masterProduct.isAvailableInStock()) {
 				return true;
