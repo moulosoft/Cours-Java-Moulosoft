2fc6666036c9fb567d699d848b34dd2d2d04d1d8
moulosoft <137833245+moulosoft@users.noreply.github.com>
2024-08-31 20:33:40 +0200

    Update PaymentValidator.java

--- src/com/itbulls/learnit/javacore/oop/interfaces/PaymentValidator.java
+++ src/com/itbulls/learnit/javacore/oop/interfaces/PaymentValidator.java
@@ -1,11 +1,20 @@
 package com.itbulls.learnit.javacore.oop.interfaces;
 
+/**
+ * Interface définissant les méthodes nécessaires pour valider un paiement.
+ */
 public interface PaymentValidator {
-	
-	boolean validatePayment(PaymentData payment);
-	
-//	default void someDefaultMethod() {
-//		System.out.println("This is another default method");
-//	};
 
+    /**
+     * Code pour valider un paiement donné.
+     *
+     * @param payment Les données du paiement à valider.
+     * @return true si le paiement est valide, false sinon.
+     */
+    boolean validatePayment(PaymentData payment);
+
+    // Méthode par défaut (optionnelle)
+//    default void someDefaultMethod() {
+//        System.out.println("This is another default method");
+//    }
 }
