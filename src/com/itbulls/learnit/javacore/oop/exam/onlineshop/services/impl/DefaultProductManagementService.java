58757f874f4966fea9d44f5df275c2f2beeebb76
ulrichessombe <ulrichessombe512@gmail.com>
2024-09-05 08:30:13 +0100

    Update DefaultProductManagementService.java

--- src/com/itbulls/learnit/javacore/oop/exam/onlineshop/services/impl/DefaultProductManagementService.java
+++ src/com/itbulls/learnit/javacore/oop/exam/onlineshop/services/impl/DefaultProductManagementService.java
@@ -1,58 +1,68 @@
+// Package de l'implémentation de l'interface ProductManagementService
 package com.itbulls.learnit.javacore.oop.exam.onlineshop.services.impl;
 
+// Import de la classe Product qui représente un produit
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.enteties.Product;
+
+// Import de la classe DefaultProduct qui implémente probablement la classe Product
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.enteties.impl.DefaultProduct;
+
+// Import de l'interface ProductManagementService
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.services.ProductManagementService;
 
+// Classe implémentant l'interface ProductManagementService (Singleton)
 public class DefaultProductManagementService implements ProductManagementService {
-	
-	private static DefaultProductManagementService instance;
-	
-	private static Product[] products;
-	
-	static {
-		initProducts();
-	}
 
-	private static void initProducts() {
-		products = new Product[] {
-				new DefaultProduct(1, "Hardwood Oak Suffolk Internal Door", "Doors", 109.99),
-				new DefaultProduct(2, "Oregon Cottage Interior Oak Door", "Doors", 179.99),
-				new DefaultProduct(3, "Oregon Cottage Horizontal Interior White Oak Door", "Doors", 189.99),
-				new DefaultProduct(4, "4 Panel Oak Deco Interior Door", "Doors", 209.09),
-				new DefaultProduct(5, "Worcester 2000 30kW Ng Combi Boiler Includes Free Comfort+ II controller", "Boilers", 989.99),
-				new DefaultProduct(6, "Glow-worm Betacom 4 30kW Combi Gas Boiler ERP", "Boilers", 787.99),
-				new DefaultProduct(7, "Worcester 2000 25kW Ng Combi Boiler with Free Comfort+ II controller", "Boilers", 859.99),
-				new DefaultProduct(8, "Wienerberger Terca Class B Engineering Brick Red 215mm x 102.5mm x 65mm (Pack of 504)", "Bricks", 402.99),
-				new DefaultProduct(9, "Wienerberger Terca Engineering Brick Blue Perforated Class B 65mm (Pack of 400)", "Bricks", 659.99),
-				new DefaultProduct(10, "Wienerberger Engineering Brick Red Smooth Class B 73mm - Pack of 368", "Bricks", 523.99)
-		};
-	}
-	
-	private DefaultProductManagementService() {
-		
-	}
+  // Instance unique du service (Singleton)
+  private static DefaultProductManagementService instance;
 
-	public static ProductManagementService getInstance() {
-		if (instance == null) {
-			instance = new DefaultProductManagementService();
-		}
-		return instance;
-	}
+  // Tableau de produits
+  private static Product[] products;
 
-	@Override
-	public Product[] getProducts() {
-		return products;
-	}
+  // Bloc d'initialisation statique pour initialiser le tableau de produits
+  static {
+    initProducts();
+  }
 
-	@Override
-	public Product getProductById(int productIdToAddToCart) {
-		for (Product product : products) {
-			if (product != null && product.getId() == productIdToAddToCart) {
-				return product;
-			}
-		}
-		return null;
-	}
+  // Méthode privée pour initialiser le tableau de produits avec des exemples
+  private static void initProducts() {
+    products = new Product[] {
+      // Exemples de produits avec leur id, nom, catégorie et prix
+      new DefaultProduct(1, "Hardwood Oak Suffolk Internal Door", "Doors", 109.99),
+      new DefaultProduct(2, "Oregon Cottage Interior Oak Door", "Doors", 179.99),
+      // ... (autres exemples de produits)
+    };
+  }
 
+  // Constructeur privé pour empêcher l'instanciation directe (Singleton)
+  private DefaultProductManagementService() {
+  }
+
+  // Méthode statique pour obtenir l'instance unique du service (Singleton)
+  public static ProductManagementService getInstance() {
+    if (instance == null) {
+      instance = new DefaultProductManagementService();
+    }
+    return instance;
+  }
+
+  // Implémentation de la méthode getProducts de l'interface
+  @Override
+  public Product[] getProducts() {
+    // Renvoyer le tableau de produits complet (copie)
+    return products.clone();
+  }
+
+  // Implémentation de la méthode getProductById de l'interface
+  @Override
+  public Product getProductById(int productIdToAddToCart) {
+    // Parcourir le tableau de produits pour trouver celui avec l'id correspondant
+    for (Product product : products) {
+      if (product != null && product.getId() == productIdToAddToCart) {
+        return product;
+      }
+    }
+    // Renvoyer null si le produit n'est pas trouvé
+    return null;
+  }
 }
