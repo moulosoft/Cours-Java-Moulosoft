efcf39ce86625ee809a528ee96e5ab4095f61a0d
ulrichessombe <ulrichessombe512@gmail.com>
2024-09-05 08:19:16 +0100

    Update OrderManagementService.java

--- src/com/itbulls/learnit/javacore/oop/exam/onlineshop/services/OrderManagementService.java
+++ src/com/itbulls/learnit/javacore/oop/exam/onlineshop/services/OrderManagementService.java
@@ -1,13 +1,20 @@
 package com.itbulls.learnit.javacore.oop.exam.onlineshop.services;
 
-import com.itbulls.learnit.javacore.oop.exam.onlineshop.enteties.Order;
+import com.itbulls.learnit.javacore.oop.exam.onlineshop.entities.Order;
 
+// Interface définissant les opérations possibles sur les commandes
 public interface OrderManagementService {
 
-	void addOrder(Order order);
+    // Méthode pour ajouter une nouvelle commande
+    // Prend en paramètre un objet Order représentant la commande à ajouter
+    void addOrder(Order order);
 
-	Order[] getOrdersByUserId(int userId);
-	
-	Order[] getOrders();
+    // Méthode pour récupérer toutes les commandes d'un utilisateur
+    // Prend en paramètre l'identifiant de l'utilisateur (userId)
+    // Retourne un tableau d'objets Order contenant les commandes de l'utilisateur
+    Order[] getOrdersByUserId(int userId);
 
+    // Méthode pour récupérer toutes les commandes
+    // Retourne un tableau d'objets Order contenant toutes les commandes
+    Order[] getOrders();
 }
