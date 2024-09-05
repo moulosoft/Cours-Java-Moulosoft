c3894a89f6841f766b9955a9c66c91bd22938806
ulrichessombe <ulrichessombe512@gmail.com>
2024-09-05 16:12:36 +0100

    Update SignInMenu.java

--- src/com/itbulls/learnit/javacore/oop/exam/onlineshop/menu/impl/SignInMenu.java
+++ src/com/itbulls/learnit/javacore/oop/exam/onlineshop/menu/impl/SignInMenu.java
@@ -8,40 +8,53 @@ import com.itbulls.learnit.javacore.oop.exam.onlineshop.menu.Menu;
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.services.UserManagementService;
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.services.impl.DefaultUserManagementService;
 
+/**
+ * Cette classe implémente l'interface Menu et représente le menu de connexion.
+ */
 public class SignInMenu implements Menu {
 
-	private ApplicationContext context;
-	private UserManagementService userManagementService;
+    // Contexte de l'application injecté
+    private ApplicationContext context;
 
-	{
-		context = ApplicationContext.getInstance();
-		userManagementService = DefaultUserManagementService.getInstance();
-	}
+    // Service de gestion des utilisateurs injecté
+    private UserManagementService userManagementService;
 
-	@Override
-	public void start() {
-		printMenuHeader();
-		Scanner sc = new Scanner(System.in);
+    // Bloc d'initialisation pour récupérer le contexte de l'application et le service de gestion des utilisateurs
+    {
+        context = ApplicationContext.getInstance();
+        userManagementService = DefaultUserManagementService.getInstance();
+    }
 
-		System.out.print("Please, enter your email: ");
-		String userEmail = sc.next();
+    @Override
+    public void start() {
+        // Affiche l'en-tête du menu
+        printMenuHeader();
 
-		System.out.print("Please, enter your password: ");
-		String userPassword = sc.next();
+        Scanner sc = new Scanner(System.in);
 
-		User user = userManagementService.getUserByEmail(userEmail);
-		if (user != null && user.getPassword().equals(userPassword)) {
-			System.out.printf("Glad to see you back %s %s", user.getFirstName(),
-					user.getLastName() + System.lineSeparator());
-			context.setLoggedInUser(user);
-		} else {
-			System.out.println("Unfortunately, such login and password doesn't exist");
-		}
-	}
+        // Demande l'adresse e-mail à l'utilisateur
+        System.out.print("Veuillez saisir votre adresse e-mail: ");
+        String userEmail = sc.next();
 
-	@Override
-	public void printMenuHeader() {
-		System.out.println("***** Sign In *****");		
-	}
+        // Demande le mot de passe à l'utilisateur
+        System.out.print("Veuillez saisir votre mot de passe: ");
+        String userPassword = sc.next();
 
+        // Recherche l'utilisateur par son adresse e-mail
+        User user = userManagementService.getUserByEmail(userEmail);
+
+        // Vérifie si l'utilisateur existe et si le mot de passe correspond
+        if (user != null && user.getPassword().equals(userPassword)) {
+            System.out.printf("Ravi de vous revoir %s %s%n", user.getFirstName(),
+                    user.getLastName());
+            context.setLoggedInUser(user); // Enregistre l'utilisateur connecté dans le contexte de l'application
+        } else {
+            System.out.println("Identifiant et mot de passe incorrects");
+        }
+    }
+
+    @Override
+    public void printMenuHeader() {
+        System.out.println("***** CONNEXION *****");
+    }
 }
