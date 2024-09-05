18fc2d833a0a782b798cc3c14f971e84ee0a64c3
ulrichessombe <ulrichessombe512@gmail.com>
2024-09-05 08:16:04 +0100

    Update UserManagementService.java

--- src/com/itbulls/learnit/javacore/oop/exam/onlineshop/services/UserManagementService.java
+++ src/com/itbulls/learnit/javacore/oop/exam/onlineshop/services/UserManagementService.java
@@ -1,13 +1,20 @@
 package com.itbulls.learnit.javacore.oop.exam.onlineshop.services;
 
-import com.itbulls.learnit.javacore.oop.exam.onlineshop.enteties.User;
+import com.itbulls.learnit.javacore.oop.exam.onlineshop.entities.User;
 
+// Interface définissant les opérations possibles sur les utilisateurs
 public interface UserManagementService {
 
-	String registerUser(User user);
-	
-	User[] getUsers();
+    // Méthode pour enregistrer un nouvel utilisateur
+    // Retourne une chaîne de caractères, probablement un message de confirmation ou un identifiant
+    String registerUser(User user);
 
-	User getUserByEmail(String userEmail);
+    // Méthode pour récupérer tous les utilisateurs
+    // Retourne un tableau d'objets User, contenant tous les utilisateurs enregistrés
+    User[] getUsers();
 
+    // Méthode pour récupérer un utilisateur par son adresse email
+    // Prend en paramètre une adresse email de type String
+    // Retourne un objet User correspondant à l'adresse email, ou null si l'utilisateur n'est pas trouvé
+    User getUserByEmail(String userEmail);
 }
