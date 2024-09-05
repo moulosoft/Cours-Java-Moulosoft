4c2e770731b381c7599b2bfc657a1b9f668ea59c
ulrichessombe <ulrichessombe512@gmail.com>
2024-09-05 16:39:43 +0100

    Update DefaultCart.java

--- src/com/itbulls/learnit/javacore/oop/exam/templates/onlineshop/enteties/impl/DefaultCart.java
+++ src/com/itbulls/learnit/javacore/oop/exam/templates/onlineshop/enteties/impl/DefaultCart.java
@@ -1,34 +1,43 @@
 package com.itbulls.learnit.javacore.oop.exam.templates.onlineshop.enteties.impl;
 
-import com.itbulls.learnit.javacore.oop.exam.onlineshop.enteties.Cart;
-import com.itbulls.learnit.javacore.oop.exam.onlineshop.enteties.Product;
+import com.itbulls.learnit.javacore.oop.exam.templates.onlineshop.enteties.Cart;
+import com.itbulls.learnit.javacore.oop.exam.templates.onlineshop.enteties.Product;
 
+/**
+ * Cette classe implémente l'interface Cart et représente un panier d'achat.
+ */
 public class DefaultCart implements Cart {
 
-	// <write your code here>
-	
-	@Override
-	public boolean isEmpty() {
-		// <write your code here>
-		
-		return true;
-	}
+    // <write your code here> // Ajoutez ici les attributs nécessaires pour représenter un panier
 
-	@Override
-	public void addProduct(Product product) {
-		// <write your code here>
-	}
+    @Override
+    public boolean isEmpty() {
+        // Vérifie si le panier est vide
+        // Implémentez ici la logique pour déterminer si le panier contient des produits
+        // Par exemple, vous pouvez utiliser un tableau ou une liste pour stocker les produits,
+        // et vérifier si le tableau ou la liste est vide.
+        return true; // Remplacez ce 'true' par la condition appropriée
+    }
 
-	@Override
-	public Product[] getProducts() {
-		// <write your code here>
-		
-		return null;
-	}
+    @Override
+    public void addProduct(Product product) {
+        // Ajoute un produit au panier
+        // Implémentez ici la logique pour ajouter le produit au panier
+        // Vous pouvez utiliser un tableau ou une liste pour stocker les produits.
+    }
 
-	@Override
-	public void clear() {
-		// <write your code here>
-	}
+    @Override
+    public Product[] getProducts() {
+        // Récupère les produits contenus dans le panier
+        // Implémentez ici la logique pour récupérer les produits du panier
+        // Vous pouvez retourner un tableau contenant les produits stockés dans le panier.
+        return null; // Remplacez ce 'null' par le tableau de produits approprié
+    }
 
+    @Override
+    public void clear() {
+        // Vide le panier
+        // Implémentez ici la logique pour vider le panier
+        // Par exemple, vous pouvez réinitialiser le tableau ou la liste utilisé pour stocker les produits.
+    }
 }
