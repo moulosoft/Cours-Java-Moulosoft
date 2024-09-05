ed1b4e388d861c3d35b6cc772656a36f453ae861
ulrichessombe <ulrichessombe512@gmail.com>
2024-09-05 16:02:41 +0100

    Update CustomerListMenu.java

--- src/com/itbulls/learnit/javacore/oop/exam/onlineshop/menu/impl/CustomerListMenu.java
+++ src/com/itbulls/learnit/javacore/oop/exam/onlineshop/menu/impl/CustomerListMenu.java
@@ -6,33 +6,44 @@ import com.itbulls.learnit.javacore.oop.exam.onlineshop.menu.Menu;
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.services.UserManagementService;
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.services.impl.DefaultUserManagementService;
 
+/**
+ * Cette classe implémente l'interface Menu et représente le menu de liste des clients.
+ */
 public class CustomerListMenu implements Menu {
 
-	private ApplicationContext context;
-	private UserManagementService userManagementService;
-	
-	{
-		userManagementService = DefaultUserManagementService.getInstance();
-		context = ApplicationContext.getInstance();
-	}
-	
-	@Override
-	public void start() {
-		printMenuHeader();
-		User[] users = userManagementService.getUsers();
-		
-		if (users.length == 0) {
-			System.out.println("Unfortunately, there are no customers.");
-		} else {
-			for (User user : users) {
-				System.out.println(user);
-			}
-		}
-	}
+    // Service de gestion des utilisateurs injecté
+    private UserManagementService userManagementService;
 
-	@Override
-	public void printMenuHeader() {
-		System.out.println("***** USERS *****");		
-	}
+    // Contexte de l'application injecté
+    private ApplicationContext context;
 
+    // Bloc d'initialisation pour récupérer le service de gestion des utilisateurs et le contexte de l'application
+    {
+        userManagementService = DefaultUserManagementService.getInstance();
+        context = ApplicationContext.getInstance();
+    }
+
+    @Override
+    public void start() {
+        // Affiche l'en-tête du menu
+        printMenuHeader();
+
+        // Récupère la liste des utilisateurs via le service de gestion des utilisateurs
+        User[] users = userManagementService.getUsers();
+
+        // Vérifie si la liste est vide
+        if (users.length == 0) {
+            System.out.println("Aucun client n'est enregistré pour le moment.");
+        } else {
+            // Parcourt la liste des utilisateurs et affiche chacun d'eux
+            for (User user : users) {
+                System.out.println(user); // On suppose que la classe User possède une implémentation toString() pour afficher les informations de l'utilisateur.
+            }
+        }
+    }
+
+    @Override
+    public void printMenuHeader() {
+        System.out.println("***** CLIENTS *****");
+    }
 }
