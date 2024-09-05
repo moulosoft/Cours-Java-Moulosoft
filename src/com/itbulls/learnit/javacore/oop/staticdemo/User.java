d09c5c540fd4b6f36d15f0ca238950cf3c42300d
moulosoft <137833245+moulosoft@users.noreply.github.com>
2024-09-06 00:31:29 +0100

    Update User.java

--- src/com/itbulls/learnit/javacore/oop/staticdemo/User.java
+++ src/com/itbulls/learnit/javacore/oop/staticdemo/User.java
@@ -1,44 +1,93 @@
-package com.itbulls.learnit.javacore.oop.staticdemo;
-
 public class User {
+// This declares the `User` class. It is the blueprint for creating `User` objects.
 
-	public static final String DEFAULT_USER_GROUP = "customers";
-	
-	private static int counter;
-	
-	private int id;
-	private String name;
-	
-	{
-		++counter;
-//		DEFAULT_USER_GROUP = "employees";
-	}
+    public static final String DEFAULT_USER_GROUP = "customers";
+    // This is a public static final variable. It means:
+    // - `public`: Accessible from anywhere.
+    // - `static`: Belongs to the `User` class itself, not individual instances.
+    // - `final`: Its value cannot be changed once assigned.
+    // `DEFAULT_USER_GROUP` holds the constant value "customers".
 
-	public static void doAnyStaticAction() {
-		System.out.println("Static method in User class is called.");
-	}
-	
-	public int getCounter() {
-		return counter;
-	}
+    private static int counter;
+    // This is a private static variable. It means:
+    // - `private`: Accessible only within the `User` class.
+    // - `static`: Shared among all instances of the `User` class.
+    // `counter` keeps track of how many `User` objects have been created.
 
-	public static void setCounter(int counter) {
-		User.counter = counter;
-	}
+    private int id;
+    // This is a private instance variable. It means:
+    // - `private`: Accessible only within the `User` class.
+    // `id` is unique to each `User` object.
 
-	public int getId() {
-		return id;
-	}
+    private String name;
+    // This is a private instance variable. It means:
+    // `name` is unique to each `User` object.
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    {
+        ++counter;
+        // This is an instance initialization block. It runs every time a new `User` object is created.
+        // `++counter` increments the static `counter` variable by 1, tracking the number of `User` objects created.
 
-	public String getName() {
-		return name;
-	}
+        // DEFAULT_USER_GROUP = "employees";
+        // This line is commented out. If uncommented, it would cause an error because `DEFAULT_USER_GROUP` is final and cannot be changed.
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public static void doAnyStaticAction() {
+        // This is a public static method. It means:
+        // - `public`: Accessible from anywhere.
+        // - `static`: Belongs to the `User` class itself.
+        // This method can be called directly on the class, like `User.doAnyStaticAction()`.
+        
+        System.out.println("Static method in User class is called.");
+        // This line prints a message to the console when the static method is called.
+    }
+
+    public int getCounter() {
+        // This is a public instance method. It means:
+        // - `public`: Accessible from anywhere.
+        // This method returns the current value of the static `counter` variable.
+
+        return counter;
+    }
+
+    public static void setCounter(int counter) {
+        // This is a public static method. It means:
+        // - `public`: Accessible from anywhere.
+        // - `static`: Belongs to the `User` class itself.
+        // This method allows setting the value of the static `counter` variable.
+
+        User.counter = counter;
+    }
+
+    public int getId() {
+        // This is a public instance method. It means:
+        // - `public`: Accessible from anywhere.
+        // This method returns the value of the `id` instance variable.
+
+        return id;
+    }
+
+    public void setId(int id) {
+        // This is a public instance method. It means:
+        // - `public`: Accessible from anywhere.
+        // This method sets the value of the `id` instance variable.
+
+        this.id = id;
+    }
+
+    public String getName() {
+        // This is a public instance method. It means:
+        // - `public`: Accessible from anywhere.
+        // This method returns the value of the `name` instance variable.
+
+        return name;
+    }
+
+    public void setName(String name) {
+        // This is a public instance method. It means:
+        // - `public`: Accessible from anywhere.
+        // This method sets the value of the `name` instance variable.
+
+        this.name = name;
+    }
 }
