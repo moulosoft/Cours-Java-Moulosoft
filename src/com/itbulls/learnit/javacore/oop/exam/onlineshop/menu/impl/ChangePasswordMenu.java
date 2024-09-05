4bf47e56049c0080ee8344144ea85df5395c04c5
ulrichessombe <ulrichessombe512@gmail.com>
2024-09-05 15:57:58 +0100

    Update ChangePasswordMenu.java

--- src/com/itbulls/learnit/javacore/oop/exam/onlineshop/menu/impl/ChangePasswordMenu.java
+++ src/com/itbulls/learnit/javacore/oop/exam/onlineshop/menu/impl/ChangePasswordMenu.java
@@ -5,27 +5,38 @@ import java.util.Scanner;
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.configs.ApplicationContext;
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.menu.Menu;
 
+/**
+ * Cette classe implémente l'interface Menu et représente le menu de changement de mot de passe.
+ */
 public class ChangePasswordMenu implements Menu {
-	
-	private ApplicationContext context;
-	
-	{
-		context = ApplicationContext.getInstance();
-	}
 
-	@Override
-	public void start() {
-		printMenuHeader();
-		Scanner sc = new Scanner(System.in);
-		String userInput = sc.next();
-		context.getLoggedInUser().setPassword(userInput);
-		System.out.println("Your password has been successfully changed");
-	}
+    // Contexte de l'application injecté
+    private ApplicationContext context;
 
-	@Override
-	public void printMenuHeader() {
-		System.out.println("***** CHANGE PASSWORD *****");
-		System.out.print("Enter new password: ");		
-	}
+    // Bloc d'initialisation pour récupérer le contexte de l'application
+    {
+        context = ApplicationContext.getInstance();
+    }
 
+    @Override
+    public void start() {
+        // Affiche l'en-tête du menu
+        printMenuHeader();
+
+        // Lit l'entrée de l'utilisateur pour le nouveau mot de passe
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.next();
+
+        // Modifie le mot de passe de l'utilisateur connecté via le contexte de l'application
+        context.getLoggedInUser().setPassword(userInput);
+
+        // Affiche un message de confirmation
+        System.out.println("Votre mot de passe a été modifié avec succès");
+    }
+
+    @Override
+    public void printMenuHeader() {
+        System.out.println("***** CHANGER LE MOT DE PASSE *****");
+        System.out.print("Entrez votre nouveau mot de passe: ");
+    }
 }
