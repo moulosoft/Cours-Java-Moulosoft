c9aa953c17345dfd1fd455b43236b479ca9b1b83
ulrichessombe <ulrichessombe512@gmail.com>
2024-09-05 16:34:49 +0100

    Update ApplicationContext.java

--- src/com/itbulls/learnit/javacore/oop/exam/templates/onlineshop/configs/ApplicationContext.java
+++ src/com/itbulls/learnit/javacore/oop/exam/templates/onlineshop/configs/ApplicationContext.java
@@ -5,48 +5,66 @@ import com.itbulls.learnit.javacore.oop.exam.onlineshop.enteties.User;
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.enteties.impl.DefaultCart;
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.menu.Menu;
 
+/**
+ * Cette classe représente le contexte de l'application.
+ * Elle permet d'accéder à des informations et objets partagés 
+ * à travers toute l'application.
+ */
 public class ApplicationContext {
-	
-	private static ApplicationContext instance;
-	
-	private User loggedInUser;
-	private Menu mainMenu;
-	private Cart sessionCart;
-	
-	private ApplicationContext() {
-	}
-	
-	public void setLoggedInUser(User user) {
-		if (this.sessionCart != null) {
-			this.sessionCart.clear(); // we have to clear session cart when new user is logged in
-		}
-		this.loggedInUser = user;
-	}
-	
-	public User getLoggedInUser() {
-		return this.loggedInUser;
-	}
-	
-	public void setMainMenu(Menu menu) {
-		this.mainMenu = menu;
-	}
-	
-	public Menu getMainMenu() {
-		return this.mainMenu;
-	}
 
-	public static ApplicationContext getInstance() {
-		if (instance == null) {
-			instance = new ApplicationContext();
-		}
-		return instance;
-	}
+    // Instance unique du contexte de l'application (Singleton)
+    private static ApplicationContext instance;
 
-	public Cart getSessionCart() {
-		if (this.sessionCart == null) {
-			this.sessionCart = new DefaultCart();
-		}
-		return this.sessionCart;
-	}
+    // Utilisateur connecté actuellement
+    private User loggedInUser;
 
+    // Menu principal de l'application
+    private Menu mainMenu;
+
+    // Panier de l'utilisateur en cours de session
+    private Cart sessionCart;
+
+    // Constructeur privé pour empêcher l'instanciation directe (Singleton)
+    private ApplicationContext() {
+    }
+
+    // Définit l'utilisateur connecté
+    public void setLoggedInUser(User user) {
+        // Vide le panier de la session s'il existe lors de la connexion d'un nouvel utilisateur
+        if (this.sessionCart != null) {
+            this.sessionCart.clear();
+        }
+        this.loggedInUser = user;
+    }
+
+    // Récupère l'utilisateur connecté
+    public User getLoggedInUser() {
+        return this.loggedInUser;
+    }
+
+    // Définit le menu principal
+    public void setMainMenu(Menu menu) {
+        this.mainMenu = menu;
+    }
+
+    // Récupère le menu principal
+    public Menu getMainMenu() {
+        return this.mainMenu;
+    }
+
+    // Récupère l'instance unique du contexte de l'application (Singleton)
+    public static ApplicationContext getInstance() {
+        if (instance == null) {
+            instance = new ApplicationContext();
+        }
+        return instance;
+    }
+
+    // Récupère le panier de la session. Crée un nouveau panier si inexistant.
+    public Cart getSessionCart() {
+        if (this.sessionCart == null) {
+            this.sessionCart = new DefaultCart();
+        }
+        return this.sessionCart;
+    }
 }
