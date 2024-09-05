9901c3c41b20a9dd36ab4218be6ccc3e6202abc4
ulrichessombe <ulrichessombe512@gmail.com>
2024-09-05 15:49:56 +0100

    Update Product.java

--- src/com/itbulls/learnit/javacore/oop/exam/onlineshop/enteties/Product.java
+++ src/com/itbulls/learnit/javacore/oop/exam/onlineshop/enteties/Product.java
@@ -1,9 +1,22 @@
 package com.itbulls.learnit.javacore.oop.exam.onlineshop.enteties;
 
+/**
+ * Cette interface définit le contrat d'un produit.
+ * Elle spécifie les méthodes de base qu'un produit doit implémenter.
+ */
 public interface Product {
 
-	int getId();
+    /**
+     * Récupère l'identifiant unique du produit.
+     *
+     * @return l'identifiant du produit.
+     */
+    int getId();
 
-	String getProductName();
-
+    /**
+     * Récupère le nom du produit.
+     *
+     * @return le nom du produit.
+     */
+    String getProductName();
 }
