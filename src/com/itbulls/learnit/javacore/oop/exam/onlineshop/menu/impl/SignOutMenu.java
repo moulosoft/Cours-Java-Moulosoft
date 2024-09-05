d8e5bb8dab766f26ee132b76cba5c1fe9baaffa9
ulrichessombe <ulrichessombe512@gmail.com>
2024-09-05 16:23:27 +0100

    Update SignOutMenu.java

--- src/com/itbulls/learnit/javacore/oop/exam/onlineshop/menu/impl/SignOutMenu.java
+++ src/com/itbulls/learnit/javacore/oop/exam/onlineshop/menu/impl/SignOutMenu.java
@@ -3,24 +3,31 @@ package com.itbulls.learnit.javacore.oop.exam.onlineshop.menu.impl;
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.configs.ApplicationContext;
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.menu.Menu;
 
+/**
+ * Cette classe implémente l'interface Menu et représente le menu de déconnexion.
+ */
 public class SignOutMenu implements Menu {
 
-	private ApplicationContext context;
-	
-	{
-		context = ApplicationContext.getInstance();
-	}
-	
-	@Override
-	public void start() {
-		printMenuHeader();
-		context.setLoggedInUser(null);
-	}
+    // Contexte de l'application injecté
+    private ApplicationContext context;
 
-	@Override
-	public void printMenuHeader() {
-		System.out.println("***** Sign Out *****");
-		System.out.println("Have a nice day! Look forward to welcoming back!");		
-	}
+    // Bloc d'initialisation pour récupérer le contexte de l'application
+    {
+        context = ApplicationContext.getInstance();
+    }
 
+    @Override
+    public void start() {
+        // Affiche l'en-tête du menu
+        printMenuHeader();
+
+        // Déconnexion de l'utilisateur
+        context.setLoggedInUser(null);
+    }
+
+    @Override
+    public void printMenuHeader() {
+        System.out.println("***** DÉCONNEXION *****");
+        System.out.println("Passez une bonne journée! Au plaisir de vous revoir! ");
+    }
 }
