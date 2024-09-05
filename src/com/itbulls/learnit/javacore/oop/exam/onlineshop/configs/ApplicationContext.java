7d64b972b09065ffbeb8ff933034835214e2aa7e
ulrichessombe <ulrichessombe512@gmail.com>
2024-09-05 08:45:57 +0100

    Update ApplicationContext.java

--- src/com/itbulls/learnit/javacore/oop/exam/onlineshop/configs/ApplicationContext.java
+++ src/com/itbulls/learnit/javacore/oop/exam/onlineshop/configs/ApplicationContext.java
@@ -1,52 +1,69 @@
+// Package de configuration de l'application
 package com.itbulls.learnit.javacore.oop.exam.onlineshop.configs;
 
+// Imports des classes nécessaires
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.enteties.Cart;
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.enteties.User;
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.enteties.impl.DefaultCart;
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.menu.Menu;
 
+// Classe ApplicationContext (Singleton) gérant le contexte de l'application
 public class ApplicationContext {
-	
-	private static ApplicationContext instance;
-	
-	private User loggedInUser;
-	private Menu mainMenu;
-	private Cart sessionCart;
-	
-	private ApplicationContext() {
-	}
-	
-	public void setLoggedInUser(User user) {
-		if (this.sessionCart != null) {
-			this.sessionCart.clear(); // we have to clear session cart when new user is logged in
-		}
-		this.loggedInUser = user;
-	}
-	
-	public User getLoggedInUser() {
-		return this.loggedInUser;
-	}
-	
-	public void setMainMenu(Menu menu) {
-		this.mainMenu = menu;
-	}
-	
-	public Menu getMainMenu() {
-		return this.mainMenu;
-	}
 
-	public static ApplicationContext getInstance() {
-		if (instance == null) {
-			instance = new ApplicationContext();
-		}
-		return instance;
-	}
+  // Instance unique du contexte de l'application (Singleton)
+  private static ApplicationContext instance;
 
-	public Cart getSessionCart() {
-		if (this.sessionCart == null) {
-			this.sessionCart = new DefaultCart();
-		}
-		return this.sessionCart;
-	}
+  // Utilisateur actuellement connecté
+  private User loggedInUser;
 
+  // Menu principal de l'application
+  private Menu mainMenu;
+
+  // Panier d'achats de la session courante
+  private Cart sessionCart;
+
+  // Constructeur privé pour empêcher l'instanciation directe (Singleton)
+  private ApplicationContext() {
+  }
+
+  // Méthode pour définir l'utilisateur connecté
+  public void setLoggedInUser(User user) {
+    // Vider le panier de la session courante lors de la connexion d'un nouvel utilisateur
+    if (this.sessionCart != null) {
+      this.sessionCart.clear();
+    }
+    this.loggedInUser = user;
+  }
+
+  // Méthode pour récupérer l'utilisateur connecté
+  public User getLoggedInUser() {
+    return this.loggedInUser;
+  }
+
+  // Méthode pour définir le menu principal
+  public void setMainMenu(Menu menu) {
+    this.mainMenu = menu;
+  }
+
+  // Méthode pour récupérer le menu principal
+  public Menu getMainMenu() {
+    return this.mainMenu;
+  }
+
+  // Méthode statique pour obtenir l'instance unique du contexte de l'application (Singleton)
+  public static ApplicationContext getInstance() {
+    if (instance == null) {
+      instance = new ApplicationContext();
+    }
+    return instance;
+  }
+
+  // Méthode pour récupérer le panier de la session courante
+  // Crée un nouveau panier par défaut si aucun n'existe
+  public Cart getSessionCart() {
+    if (this.sessionCart == null) {
+      this.sessionCart = new DefaultCart();
+    }
+    return this.sessionCart;
+  }
 }
