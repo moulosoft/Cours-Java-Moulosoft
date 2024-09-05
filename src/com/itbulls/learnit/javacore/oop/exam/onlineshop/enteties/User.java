04920a6d3bcfc2e83b6fd7fe47942d4d6f330ca0
ulrichessombe <ulrichessombe512@gmail.com>
2024-09-05 15:51:17 +0100

    Update User.java

--- src/com/itbulls/learnit/javacore/oop/exam/onlineshop/enteties/User.java
+++ src/com/itbulls/learnit/javacore/oop/exam/onlineshop/enteties/User.java
@@ -1,15 +1,57 @@
 package com.itbulls.learnit.javacore.oop.exam.onlineshop.enteties;
 
+/**
+ * Cette interface définit le contrat d'un utilisateur.
+ * Elle spécifie les méthodes de base qu'un utilisateur doit implémenter.
+ */
 public interface User {
-	
-	String getFirstName();
-	String getLastName();
-	String getPassword();
-	String getEmail();
-	int getId();
-	
-	void setPassword(String newPassword);
-	void setEmail(String newEmail);
-	
-	
+
+    /**
+     * Récupère le prénom de l'utilisateur.
+     *
+     * @return le prénom de l'utilisateur.
+     */
+    String getFirstName();
+
+    /**
+     * Récupère le nom de famille de l'utilisateur.
+     *
+     * @return le nom de famille de l'utilisateur.
+     */
+    String getLastName();
+
+    /**
+     * Récupère le mot de passe de l'utilisateur.
+     *
+     * @return le mot de passe de l'utilisateur.
+     */
+    String getPassword();
+
+    /**
+     * Récupère l'adresse email de l'utilisateur.
+     *
+     * @return l'adresse email de l'utilisateur.
+     */
+    String getEmail();
+
+    /**
+     * Récupère l'identifiant unique de l'utilisateur.
+     *
+     * @return l'identifiant de l'utilisateur.
+     */
+    int getId();
+
+    /**
+     * Définit le nouveau mot de passe de l'utilisateur.
+     *
+     * @param newPassword le nouveau mot de passe.
+     */
+    void setPassword(String newPassword);
+
+    /**
+     * Définit la nouvelle adresse email de l'utilisateur.
+     *
+     * @param newEmail la nouvelle adresse email.
+     */
+    void setEmail(String newEmail);
 }
