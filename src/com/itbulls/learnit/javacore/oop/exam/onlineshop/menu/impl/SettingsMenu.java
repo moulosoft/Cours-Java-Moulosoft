3a4c42f2b142d46b50f06bae8716d3473096f780
ulrichessombe <ulrichessombe512@gmail.com>
2024-09-05 16:10:23 +0100

    Update SettingsMenu.java

--- src/com/itbulls/learnit/javacore/oop/exam/onlineshop/menu/impl/SettingsMenu.java
+++ src/com/itbulls/learnit/javacore/oop/exam/onlineshop/menu/impl/SettingsMenu.java
@@ -5,61 +5,80 @@ import java.util.Scanner;
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.configs.ApplicationContext;
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.menu.Menu;
 
+/**
+ * Cette classe implémente l'interface Menu et représente le menu des paramètres de l'utilisateur.
+ */
 public class SettingsMenu implements Menu {
 
-	private static final String SETTINGS = "1. Change Password" + System.lineSeparator()
-			+ "2. Change Email";
+    // Constante contenant les options du menu des paramètres
+    private static final String SETTINGS = "1. Modifier le mot de passe" + System.lineSeparator()
+            + "2. Modifier l'adresse e-mail";
 
-	private ApplicationContext context;
+    // Contexte de l'application injecté
+    private ApplicationContext context;
 
-	{
-		context = ApplicationContext.getInstance();
-	}
+    // Bloc d'initialisation pour récupérer le contexte de l'application
+    {
+        context = ApplicationContext.getInstance();
+    }
 
-	@Override
-	public void start() {
-		Menu menuToNavigate = null;
-		mainLoop: while (true) {
-			printMenuHeader();
-			if (context.getLoggedInUser() == null) {
-				System.out.println(
-						"Please, log in or create new account to change your account settings");
-				new MainMenu().start();
-				return;
-			} else {
-				System.out.println(SETTINGS);
-				System.out.print(
-						"Please, enter option or type 'menu' to navigate back to the main menu: ");
-				Scanner sc = new Scanner(System.in);
-				String userInput = sc.next();
+    @Override
+    public void start() {
+        // Variable pour stocker le menu à lancer
+        Menu menuToNavigate = null;
 
-				if (userInput.equalsIgnoreCase(MainMenu.MENU_COMMAND)) {
-					menuToNavigate = new MainMenu();
-					break mainLoop;
-				}
+        // Boucle infinie pour la navigation dans le menu
+        mainLoop:
+        while (true) {
+            // Affiche l'en-tête du menu
+            printMenuHeader();
 
-				int userOption = Integer.parseInt(userInput);
-				switch (userOption) {
-				case 1:
-					menuToNavigate = new ChangePasswordMenu();
-					break mainLoop;
-				case 2:
-					menuToNavigate = new ChangeEmailMenu();
-					break mainLoop;
-				default:
-					System.out.println("Only 1, 2 is allowed. Try one more time");
-					continue;
-				}
-			}
-		}
+            // Vérifie si l'utilisateur est connecté
+            if (context.getLoggedInUser() == null) {
+                System.out.println(
+                        "Veuillez vous connecter ou créer un nouveau compte pour modifier vos paramètres");
+                new MainMenu().start();
+                return;
+            } else {
+                // Affiche les options du menu des paramètres
+                System.out.println(SETTINGS);
 
-		menuToNavigate.start();
+                Scanner sc = new Scanner(System.in);
 
-	}
+                System.out.print(
+                        "Veuillez saisir une option ou taper 'menu' pour revenir au menu principal: ");
+                String userInput = sc.next();
 
-	@Override
-	public void printMenuHeader() {
-		System.out.println("***** SETTINGS *****");		
-	}
+                // Gère la navigation vers le menu principal
+                if (userInput.equalsIgnoreCase(MainMenu.MENU_COMMAND)) {
+                    menuToNavigate = new MainMenu();
+                    break mainLoop;
+                }
 
+                // Convertit la saisie en entier
+                int userOption = Integer.parseInt(userInput);
+
+                // Gère les différentes options du menu
+                switch (userOption) {
+                    case 1:
+                        menuToNavigate = new ChangePasswordMenu();
+                        break mainLoop; // Sort de la boucle pour lancer le menu de modification du mot de passe
+                    case 2:
+                        menuToNavigate = new ChangeEmailMenu();
+                        break mainLoop; // Sort de la boucle pour lancer le menu de modification de l'adresse e-mail
+                    default:
+                        System.out.println("Seulement 1 et 2 sont autorisés. Veuillez réessayer");
+                        continue; // Continue la boucle pour une nouvelle saisie
+                }
+            }
+        }
+
+        // Lance le menu sélectionné
+        menuToNavigate.start();
+    }
+
+    @Override
+    public void printMenuHeader() {
+        System.out.println("***** PARAMETRES *****");
+    }
 }
