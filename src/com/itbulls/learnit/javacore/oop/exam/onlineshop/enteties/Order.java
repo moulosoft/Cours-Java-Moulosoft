90eb2d525eb349e53e2e23fb177a04c9ee7bbeca
ulrichessombe <ulrichessombe512@gmail.com>
2024-09-05 09:00:56 +0100

    Update Order.java

--- src/com/itbulls/learnit/javacore/oop/exam/onlineshop/enteties/Order.java
+++ src/com/itbulls/learnit/javacore/oop/exam/onlineshop/enteties/Order.java
@@ -1,15 +1,44 @@
 package com.itbulls.learnit.javacore.oop.exam.onlineshop.enteties;
 
+/**
+ * Cette interface définit le contrat d'une commande.
+ * Elle spécifie les méthodes de base qu'une commande doit implémenter.
+ */
 public interface Order {
 
-	boolean isCreditCardNumberValid(String userInput);
+    /**
+     * Vérifie la validité du numéro de carte de crédit saisi par l'utilisateur.
+     *
+     * @param userInput le numéro de carte de crédit saisi par l'utilisateur.
+     * @return true si le numéro de carte de crédit est valide, false sinon.
+     */
+    boolean isCreditCardNumberValid(String userInput);
 
-	void setCreditCardNumber(String userInput);
+    /**
+     * Définit le numéro de carte de crédit de la commande.
+     *
+     * @param userInput le numéro de carte de crédit à définir.
+     */
+    void setCreditCardNumber(String userInput);
 
-	void setProducts(Product[] products);
+    /**
+     * Définit les produits inclus dans la commande.
+     *
+     * @param products le tableau de produits à définir.
+     */
+    void setProducts(Product[] products);
 
-	void setCustomerId(int customerId);
-	
-	int getCustomerId();
+    /**
+     * Définit l'identifiant du client qui a passé la commande.
+     *
+     * @param customerId l'identifiant du client.
+     */
+    void setCustomerId(int customerId);
 
+    /**
+     * Récupère l'identifiant du client qui a passé la commande.
+     *
+     * @return l'identifiant du client.
+     */
+    int getCustomerId();
 }
