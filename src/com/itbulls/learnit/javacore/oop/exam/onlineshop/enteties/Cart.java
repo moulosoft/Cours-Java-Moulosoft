424570f50e0518f03c1c876e8bddbab591785cfb
ulrichessombe <ulrichessombe512@gmail.com>
2024-09-05 08:58:41 +0100

    Update Cart.java

--- src/com/itbulls/learnit/javacore/oop/exam/onlineshop/enteties/Cart.java
+++ src/com/itbulls/learnit/javacore/oop/exam/onlineshop/enteties/Cart.java
@@ -1,13 +1,34 @@
 package com.itbulls.learnit.javacore.oop.exam.onlineshop.enteties;
 
+/**
+ * Cette interface définit le contrat d'un panier d'achat.
+ * Elle spécifie les méthodes de base qu'un panier doit implémenter.
+ */
 public interface Cart {
 
-	boolean isEmpty();
+    /**
+     * Vérifie si le panier est vide.
+     *
+     * @return true si le panier ne contient aucun produit, false sinon.
+     */
+    boolean isEmpty();
 
-	void addProduct(Product productById);
+    /**
+     * Ajoute un produit au panier.
+     *
+     * @param productById le produit à ajouter.
+     */
+    void addProduct(Product productById);
 
-	Product[] getProducts();
+    /**
+     * Récupère tous les produits contenus dans le panier.
+     *
+     * @return un tableau contenant tous les produits du panier.
+     */
+    Product[] getProducts();
 
-	void clear();
-
+    /**
+     * Vide complètement le panier.
+     */
+    void clear();
 }
