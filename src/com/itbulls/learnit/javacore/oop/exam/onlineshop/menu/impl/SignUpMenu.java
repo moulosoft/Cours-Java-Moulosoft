b680e78ba6ef2e11f63b70458b8390a251377d8b
ulrichessombe <ulrichessombe512@gmail.com>
2024-09-05 16:25:10 +0100

    Update SignUpMenu.java

--- src/com/itbulls/learnit/javacore/oop/exam/onlineshop/menu/impl/SignUpMenu.java
+++ src/com/itbulls/learnit/javacore/oop/exam/onlineshop/menu/impl/SignUpMenu.java
@@ -9,46 +9,64 @@ import com.itbulls.learnit.javacore.oop.exam.onlineshop.menu.Menu;
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.services.UserManagementService;
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.services.impl.DefaultUserManagementService;
 
+/**
+ * Cette classe implémente l'interface Menu et représente le menu d'inscription.
+ */
 public class SignUpMenu implements Menu {
 
-	private UserManagementService userManagementService;
-	private ApplicationContext context;
+    // Service de gestion des utilisateurs injecté
+    private UserManagementService userManagementService;
 
-	{
-		userManagementService = DefaultUserManagementService.getInstance();
-		context = ApplicationContext.getInstance();
-	}
+    // Contexte de l'application injecté
+    private ApplicationContext context;
 
-	@Override
-	public void start() {
-		printMenuHeader();
-		
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Please, enter your first name: ");
-		String firstName = sc.next();
-		System.out.print("Please, enter your last name: ");
-		String lastName = sc.next();
-		System.out.print("Please, enter your password: ");
-		String password = sc.next();
-		System.out.print("Please, enter your email: ");
-		
-		sc = new Scanner(System.in);
-		String email = sc.nextLine();
+    // Bloc d'initialisation pour récupérer le service de gestion des utilisateurs et le contexte de l'application
+    {
+        userManagementService = DefaultUserManagementService.getInstance();
+        context = ApplicationContext.getInstance();
+    }
 
-		User user = new DefaultUser(firstName, lastName, password, email);
-		
-		String errorMessage = userManagementService.registerUser(user);
-		if (errorMessage == null || errorMessage.isEmpty()) {
-			context.setLoggedInUser(user);
-			System.out.println("New user is created");
-		} else {
-			System.out.println(errorMessage);
-		}
-	}
+    @Override
+    public void start() {
+        // Affiche l'en-tête du menu
+        printMenuHeader();
 
-	@Override
-	public void printMenuHeader() {
-		System.out.println("***** SIGN UP *****");		
-	}
+        Scanner sc = new Scanner(System.in);
 
+        // Demande le prénom à l'utilisateur
+        System.out.print("Veuillez saisir votre prénom: ");
+        String firstName = sc.next();
+
+        // Demande le nom de famille à l'utilisateur
+        System.out.print("Veuillez saisir votre nom de famille: ");
+        String lastName = sc.next();
+
+        // Demande le mot de passe à l'utilisateur
+        System.out.print("Veuillez saisir votre mot de passe: ");
+        String password = sc.next();
+
+        // Demande l'adresse e-mail à l'utilisateur (on utilise nextLine pour récupérer la ligne entière 
+        // car l'adresse e-mail peut contenir des espaces)
+        System.out.print("Veuillez saisir votre adresse e-mail: ");
+        String email = sc.nextLine();
+
+        // Crée un nouvel utilisateur avec les informations saisies
+        User user = new DefaultUser(firstName, lastName, password, email);
+
+        // Tente d'enregistrer l'utilisateur auprès du service de gestion des utilisateurs
+        String errorMessage = userManagementService.registerUser(user);
+
+        // Vérifie si l'enregistrement a réussi
+        if (errorMessage == null || errorMessage.isEmpty()) {
+            context.setLoggedInUser(user); // Enregistre l'utilisateur connecté dans le contexte de l'application
+            System.out.println("Nouvel utilisateur créé");
+        } else {
+            System.out.println(errorMessage); // Affiche un message d'erreur si l'enregistrement a échoué
+        }
+    }
+
+    @Override
+    public void printMenuHeader() {
+        System.out.println("***** INSCRIPTION *****");
+    }
 }
