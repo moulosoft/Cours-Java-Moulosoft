e100711b263ba4db1136c9d6f289b854c6feaf55
moulosoft <137833245+moulosoft@users.noreply.github.com>
2024-09-09 15:54:54 +0100

    Update MainMenu.java

--- src/com/itbulls/learnit/javacore/oop/exam/templates/onlineshop/menu/impl/MainMenu.java
+++ src/com/itbulls/learnit/javacore/oop/exam/templates/onlineshop/menu/impl/MainMenu.java
@@ -3,37 +3,49 @@ package com.itbulls.learnit.javacore.oop.exam.templates.onlineshop.menu.impl;
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.configs.ApplicationContext;
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.menu.Menu;
 
+/**
+ * Cette classe implémente l'interface Menu et représente le menu principal de l'application.
+ */
 public class MainMenu implements Menu {
 
-	public static final String MENU_COMMAND = "menu";
-	
-	private static final String MAIN_MENU_TEXT_FOR_LOGGED_OUT_USER = "Please, enter number in console to proceed." + System.lineSeparator()
-			+ "1. Sign Up" + System.lineSeparator() + "2. Sign In"
-			+ System.lineSeparator() + "3. Product Catalog" + System.lineSeparator()
-			+ "4. My Orders" + System.lineSeparator() + "5. Settings" + System.lineSeparator() + 
-			"6. Customer List";
+    // Commande pour accéder au menu principal (probablement saisie par l'utilisateur)
+    public static final String MENU_COMMAND = "menu";
 
-	private static final String MAIN_MENU_TEXT_FOR_LOGGED_IN_USER = "Please, enter number in console to proceed." + System.lineSeparator()
-			+ "1. Sign Up" + System.lineSeparator() + "2. Sign Out"
-			+ System.lineSeparator() + "3. Product Catalog" + System.lineSeparator()
-			+ "4. My Orders" + System.lineSeparator() + "5. Settings" + System.lineSeparator() + 
-			"6. Customer List";;
+    // Texte du menu principal pour un utilisateur non connecté
+    private static final String MAIN_MENU_TEXT_FOR_LOGGED_OUT_USER = "Veuillez entrer un numéro pour continuer :"
+            + System.lineSeparator()
+            + "1. Inscription" + System.lineSeparator()
+            + "2. Connexion" + System.lineSeparator()
+            + "3. Catalogue de produits" + System.lineSeparator()
+            + "4. Mes commandes" + System.lineSeparator()
+            + "5. Paramètres" + System.lineSeparator()
+            + "6. Liste des clients"; // Notez le point-virgule en trop corrigé
 
-	private ApplicationContext context;
-	
-	{
-		context = ApplicationContext.getInstance();
-	}
-	
-	@Override
-	public void start() {
-		// <write your code here>
-		
-	}
+    // Texte du menu principal pour un utilisateur connecté
+    private static final String MAIN_MENU_TEXT_FOR_LOGGED_IN_USER = "Veuillez entrer un numéro pour continuer :"
+            + System.lineSeparator()
+            + "1. Inscription" + System.lineSeparator() // L'option "Inscription" est présente même pour les utilisateurs connectés (probablement pour créer un nouveau compte)
+            + "2. Déconnexion" + System.lineSeparator()
+            + "3. Catalogue de produits" + System.lineSeparator()
+            + "4. Mes commandes" + System.lineSeparator()
+            + "5. Paramètres" + System.lineSeparator()
+            + "6. Liste des clients"; // Notez le point-virgule en trop corrigé
 
-	@Override
-	public void printMenuHeader() {
-		// <write your code here>
-	}
+    // Contexte de l'application injecté
+    private ApplicationContext context;
 
+    // Bloc d'initialisation pour récupérer le contexte de l'application
+    {
+        context = ApplicationContext.getInstance();
+    }
+
+    @Override
+    public void start() {
+        // <write your code here> // Code à implémenter pour afficher et gérer le menu principal
+    }
+
+    @Override
+    public void printMenuHeader() {
+        // <write your code here> // Code à implémenter pour afficher l'en-tête du menu principal
+    }
 }
