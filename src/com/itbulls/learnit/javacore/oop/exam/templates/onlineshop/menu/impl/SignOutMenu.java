5838c5aee769931bf59afb452f2dc61616b61cc9
moulosoft <137833245+moulosoft@users.noreply.github.com>
2024-09-09 16:25:34 +0100

    Update SignOutMenu.java

--- src/com/itbulls/learnit/javacore/oop/exam/templates/onlineshop/menu/impl/SignOutMenu.java
+++ src/com/itbulls/learnit/javacore/oop/exam/templates/onlineshop/menu/impl/SignOutMenu.java
@@ -3,22 +3,26 @@ package com.itbulls.learnit.javacore.oop.exam.templates.onlineshop.menu.impl;
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.configs.ApplicationContext;
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.menu.Menu;
 
+/**
+ * Cette classe implémente l'interface Menu et représente le menu de déconnexion.
+ */
 public class SignOutMenu implements Menu {
 
-	private ApplicationContext context;
-	
-	{
-		context = ApplicationContext.getInstance();
-	}
-	
-	@Override
-	public void start() {
-		// <write your code here>
-	}
+    // Contexte de l'application injecté
+    private ApplicationContext context;
 
-	@Override
-	public void printMenuHeader() {
-		// <write your code here>	
-	}
+    // Bloc d'initialisation pour récupérer le contexte de l'application
+    {
+        context = ApplicationContext.getInstance();
+    }
 
+    @Override
+    public void start() {
+        // <write your code here> // Code à implémenter pour gérer la déconnexion de l'utilisateur
+    }
+
+    @Override
+    public void printMenuHeader() {
+        // <write your code here> // Code à implémenter pour afficher l'en-tête du menu
+    }
 }
