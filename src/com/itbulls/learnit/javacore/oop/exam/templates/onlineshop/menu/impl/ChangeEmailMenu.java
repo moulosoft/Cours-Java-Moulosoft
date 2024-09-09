9357e26e985decbf7f72b00ce3aed04e42b4df45
moulosoft <137833245+moulosoft@users.noreply.github.com>
2024-09-09 15:28:14 +0100

    Update ChangeEmailMenu.java

--- src/com/itbulls/learnit/javacore/oop/exam/templates/onlineshop/menu/impl/ChangeEmailMenu.java
+++ src/com/itbulls/learnit/javacore/oop/exam/templates/onlineshop/menu/impl/ChangeEmailMenu.java
@@ -3,22 +3,26 @@ package com.itbulls.learnit.javacore.oop.exam.templates.onlineshop.menu.impl;
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.configs.ApplicationContext;
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.menu.Menu;
 
+/**
+ * Cette classe implémente l'interface Menu et représente le menu de changement d'adresse e-mail.
+ */
 public class ChangeEmailMenu implements Menu {
 
-	private ApplicationContext context;
+    // Contexte de l'application injecté
+    private ApplicationContext context;
 
-	{
-		context = ApplicationContext.getInstance();
-	}
+    // Bloc d'initialisation pour récupérer le contexte de l'application
+    {
+        context = ApplicationContext.getInstance();
+    }
 
-	@Override
-	public void start() {
-		// <write your code here>
-	}
+    @Override
+    public void start() {
+        // <write your code here> // Code à implémenter pour lancer le menu de changement d'e-mail
+    }
 
-	@Override
-	public void printMenuHeader() {
-		// <write your code here>
-	}
-
+    @Override
+    public void printMenuHeader() {
+        // <write your code here> // Code à implémenter pour afficher l'en-tête du menu
+    }
 }
