0319bc6bf1263f8f8b1ecd840ac93df5b9c3f077
moulosoft <137833245+moulosoft@users.noreply.github.com>
2024-09-09 15:34:49 +0100

    Update CustomerListMenu.java

--- src/com/itbulls/learnit/javacore/oop/exam/templates/onlineshop/menu/impl/CustomerListMenu.java
+++ src/com/itbulls/learnit/javacore/oop/exam/templates/onlineshop/menu/impl/CustomerListMenu.java
@@ -5,24 +5,31 @@ import com.itbulls.learnit.javacore.oop.exam.onlineshop.menu.Menu;
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.services.UserManagementService;
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.services.impl.DefaultUserManagementService;
 
+/**
+ * Cette classe implémente l'interface Menu et représente le menu de liste des clients.
+ * On peut supposer que ce menu est accessible uniquement aux administrateurs.
+ */
 public class CustomerListMenu implements Menu {
 
-	private ApplicationContext context;
-	private UserManagementService userManagementService;
-	
-	{
-		userManagementService = DefaultUserManagementService.getInstance();
-		context = ApplicationContext.getInstance();
-	}
-	
-	@Override
-	public void start() {
-		// <write your code here>
-	}
+    // Contexte de l'application injecté
+    private ApplicationContext context;
 
-	@Override
-	public void printMenuHeader() {
-		// <write your code here>	
-	}
+    // Service de gestion des utilisateurs
+    private UserManagementService userManagementService;
 
+    // Bloc d'initialisation pour récupérer le service de gestion des utilisateurs et le contexte de l'application
+    {
+        userManagementService = DefaultUserManagementService.getInstance();
+        context = ApplicationContext.getInstance();
+    }
+
+    @Override
+    public void start() {
+        // <write your code here> // Code à implémenter pour afficher la liste des clients
+    }
+
+    @Override
+    public void printMenuHeader() {
+        // <write your code here> // Code à implémenter pour afficher l'en-tête du menu
+    }
 }
