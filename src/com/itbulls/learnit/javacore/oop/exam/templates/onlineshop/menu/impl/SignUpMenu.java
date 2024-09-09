a2b22ed3137b716fc15316f3e1bd085ca7b72f46
moulosoft <137833245+moulosoft@users.noreply.github.com>
2024-09-09 16:27:31 +0100

    Update SignUpMenu.java

--- src/com/itbulls/learnit/javacore/oop/exam/templates/onlineshop/menu/impl/SignUpMenu.java
+++ src/com/itbulls/learnit/javacore/oop/exam/templates/onlineshop/menu/impl/SignUpMenu.java
@@ -5,24 +5,30 @@ import com.itbulls.learnit.javacore.oop.exam.onlineshop.menu.Menu;
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.services.UserManagementService;
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.services.impl.DefaultUserManagementService;
 
+/**
+ * Cette classe implémente l'interface Menu et représente le menu d'inscription.
+ */
 public class SignUpMenu implements Menu {
 
-	private UserManagementService userManagementService;
-	private ApplicationContext context;
+    // Service de gestion des utilisateurs
+    private UserManagementService userManagementService;
 
-	{
-		userManagementService = DefaultUserManagementService.getInstance();
-		context = ApplicationContext.getInstance();
-	}
+    // Contexte de l'application injecté
+    private ApplicationContext context;
 
-	@Override
-	public void start() {
-		// <write your code here>
-	}
+    // Bloc d'initialisation pour récupérer le service de gestion des utilisateurs et le contexte de l'application
+    {
+        userManagementService = DefaultUserManagementService.getInstance();
+        context = ApplicationContext.getInstance();
+    }
 
-	@Override
-	public void printMenuHeader() {
-		// <write your code here>	
-	}
+    @Override
+    public void start() {
+        // <write your code here> // Code à implémenter pour gérer l'inscription d'un nouvel utilisateur
+    }
 
+    @Override
+    public void printMenuHeader() {
+        // <write your code here> // Code à implémenter pour afficher l'en-tête du menu
+    }
 }
