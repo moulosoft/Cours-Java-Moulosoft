39525bf5a6d87e901537a9a51ff6539041a4d9cb
moulosoft <137833245+moulosoft@users.noreply.github.com>
2024-09-09 15:30:32 +0100

    Update ChangePasswordMenu.java

--- src/com/itbulls/learnit/javacore/oop/exam/templates/onlineshop/menu/impl/ChangePasswordMenu.java
+++ src/com/itbulls/learnit/javacore/oop/exam/templates/onlineshop/menu/impl/ChangePasswordMenu.java
@@ -3,22 +3,26 @@ package com.itbulls.learnit.javacore.oop.exam.templates.onlineshop.menu.impl;
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.configs.ApplicationContext;
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.menu.Menu;
 
+/**
+ * Cette classe implémente l'interface Menu et représente le menu de changement de mot de passe.
+ */
 public class ChangePasswordMenu implements Menu {
-	
-	private ApplicationContext context;
-	
-	{
-		context = ApplicationContext.getInstance();
-	}
 
-	@Override
-	public void start() {
-		// <write your code here>
-	}
+    // Contexte de l'application injecté
+    private ApplicationContext context;
 
-	@Override
-	public void printMenuHeader() {
-		// <write your code here>		
-	}
+    // Bloc d'initialisation pour récupérer le contexte de l'application
+    {
+        context = ApplicationContext.getInstance();
+    }
 
-}
+    @Override
+    public void start() {
+        // <write your code here> // Code à implémenter pour lancer le menu de changement de mot de passe
+    }
+
+    @Override
+    public void printMenuHeader() {
+        // <write your code here> // Code à implémenter pour afficher l'en-tête du menu
+    }
+}ù
