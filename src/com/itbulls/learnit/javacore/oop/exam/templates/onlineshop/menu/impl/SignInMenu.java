dedd68d32d92d8dee3a59b3183a6640bd41d8f03
moulosoft <137833245+moulosoft@users.noreply.github.com>
2024-09-09 16:01:02 +0100

    Update SignInMenu.java

--- src/com/itbulls/learnit/javacore/oop/exam/templates/onlineshop/menu/impl/SignInMenu.java
+++ src/com/itbulls/learnit/javacore/oop/exam/templates/onlineshop/menu/impl/SignInMenu.java
@@ -5,24 +5,30 @@ import com.itbulls.learnit.javacore.oop.exam.onlineshop.menu.Menu;
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.services.UserManagementService;
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.services.impl.DefaultUserManagementService;
 
+/**
+ * Cette classe implémente l'interface Menu et représente le menu de connexion.
+ */
 public class SignInMenu implements Menu {
 
-	private ApplicationContext context;
-	private UserManagementService userManagementService;
+    // Contexte de l'application injecté
+    private ApplicationContext context;
 
-	{
-		context = ApplicationContext.getInstance();
-		userManagementService = DefaultUserManagementService.getInstance();
-	}
+    // Service de gestion des utilisateurs
+    private UserManagementService userManagementService;
 
-	@Override
-	public void start() {
-		// <write your code here>
-	}
+    // Bloc d'initialisation pour récupérer le contexte de l'application et le service de gestion des utilisateurs
+    {
+        context = ApplicationContext.getInstance();
+        userManagementService = DefaultUserManagementService.getInstance();
+    }
 
-	@Override
-	public void printMenuHeader() {
-		// <write your code here>	
-	}
+    @Override
+    public void start() {
+        // <write your code here> // Code à implémenter pour gérer la connexion de l'utilisateur
+    }
 
+    @Override
+    public void printMenuHeader() {
+        // <write your code here> // Code à implémenter pour afficher l'en-tête du menu
+    }
 }
