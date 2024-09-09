0d4e5140d825f04c74ff2e881338d6e818603e24
moulosoft <137833245+moulosoft@users.noreply.github.com>
2024-09-09 15:32:53 +0100

    Update CheckoutMenu.java

--- src/com/itbulls/learnit/javacore/oop/exam/templates/onlineshop/menu/impl/CheckoutMenu.java
+++ src/com/itbulls/learnit/javacore/oop/exam/templates/onlineshop/menu/impl/CheckoutMenu.java
@@ -5,24 +5,30 @@ import com.itbulls.learnit.javacore.oop.exam.onlineshop.menu.Menu;
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.services.OrderManagementService;
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.services.impl.DefaultOrderManagementService;
 
+/**
+ * Cette classe implémente l'interface Menu et représente le menu de paiement.
+ */
 public class CheckoutMenu implements Menu {
 
-	private ApplicationContext context;
-	private OrderManagementService orderManagementService;
-	
-	{
-		context = ApplicationContext.getInstance();
-		orderManagementService = DefaultOrderManagementService.getInstance();
-	}
-	
-	@Override
-	public void start() {
-		// <write your code here>
-	}
+    // Contexte de l'application injecté
+    private ApplicationContext context;
 
-	@Override
-	public void printMenuHeader() {
-		// <write your code here>
-	}
+    // Service de gestion des commandes
+    private OrderManagementService orderManagementService;
 
+    // Bloc d'initialisation pour récupérer le contexte de l'application et le service de gestion des commandes
+    {
+        context = ApplicationContext.getInstance();
+        orderManagementService = DefaultOrderManagementService.getInstance();
+    }
+
+    @Override
+    public void start() {
+        // <write your code here> // Code à implémenter pour lancer le menu de paiement
+    }
+
+    @Override
+    public void printMenuHeader() {
+        // <write your code here> // Code à implémenter pour afficher l'en-tête du menu
+    }
 }
