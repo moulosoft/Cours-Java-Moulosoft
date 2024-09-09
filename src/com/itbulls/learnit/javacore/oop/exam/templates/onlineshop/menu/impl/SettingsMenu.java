18857254642bd6bb539b7a2f2ae444c6c647459e
moulosoft <137833245+moulosoft@users.noreply.github.com>
2024-09-09 15:59:39 +0100

    Update SettingsMenu.java

--- src/com/itbulls/learnit/javacore/oop/exam/templates/onlineshop/menu/impl/SettingsMenu.java
+++ src/com/itbulls/learnit/javacore/oop/exam/templates/onlineshop/menu/impl/SettingsMenu.java
@@ -3,25 +3,30 @@ package com.itbulls.learnit.javacore.oop.exam.templates.onlineshop.menu.impl;
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.configs.ApplicationContext;
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.menu.Menu;
 
+/**
+ * Cette classe implémente l'interface Menu et représente le menu des paramètres.
+ */
 public class SettingsMenu implements Menu {
 
-	private static final String SETTINGS = "1. Change Password" + System.lineSeparator()
-			+ "2. Change Email";
+    // Texte affiché pour les options du menu des paramètres
+    private static final String SETTINGS = "1. Modifier le mot de passe" + System.lineSeparator()
+            + "2. Modifier l'e-mail";
 
-	private ApplicationContext context;
+    // Contexte de l'application injecté
+    private ApplicationContext context;
 
-	{
-		context = ApplicationContext.getInstance();
-	}
+    // Bloc d'initialisation pour récupérer le contexte de l'application
+    {
+        context = ApplicationContext.getInstance();
+    }
 
-	@Override
-	public void start() {
-		// <write your code here>
-	}
+    @Override
+    public void start() {
+        // <write your code here> // Code à implémenter pour afficher et gérer les paramètres
+    }
 
-	@Override
-	public void printMenuHeader() {
-		// <write your code here>	
-	}
-
+    @Override
+    public void printMenuHeader() {
+        // <write your code here> // Code à implémenter pour afficher l'en-tête du menu
+    }
 }
