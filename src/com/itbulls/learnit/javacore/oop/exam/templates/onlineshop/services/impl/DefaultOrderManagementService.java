853d1a890adce86fc67bd257f454aa57188c1324
moulosoft <137833245+moulosoft@users.noreply.github.com>
2024-09-29 16:13:40 +0100

    Update DefaultOrderManagementService.java

--- src/com/itbulls/learnit/javacore/oop/exam/templates/onlineshop/services/impl/DefaultOrderManagementService.java
+++ src/com/itbulls/learnit/javacore/oop/exam/templates/onlineshop/services/impl/DefaultOrderManagementService.java
@@ -1,42 +1,41 @@
 package com.itbulls.learnit.javacore.oop.exam.templates.onlineshop.services.impl;
-
+/*Cette ligne indique le package auquel appartient ce code. Cela définit un emplacement spécifique au sein d'une structure de projet plus large. Ici, il s'agit du package impl (implémentation) du service de gestion des commandes dans un projet de boutique en ligne*/
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.enteties.Order;
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.services.OrderManagementService;
+/*Ces lignes importent deux classes d'autres packages :
 
+Order : Cette classe représente probablement un objet de commande dans le système de boutique en ligne.
+OrderManagementService : Cette interface définit des méthodes pour gérer les commandes.*/
 public class DefaultOrderManagementService implements OrderManagementService {
-
-	private static final int DEFAULT_ORDER_CAPACITY = 10;
-
-	private static DefaultOrderManagementService instance;
-
-	// <write your code here>
-	
-	public static OrderManagementService getInstance() {
-		if (instance == null) {
-			instance = new DefaultOrderManagementService();
-		}
-		return instance;
-	}
-
-	@Override
-	public void addOrder(Order order) {
-		// <write your code here>
-	}
-
-	@Override
-	public Order[] getOrdersByUserId(int userId) {
-		// <write your code here>
-		return null;
-	}
-
-	@Override
-	public Order[] getOrders() {
-		// <write your code here>
-		return null;
-	}
-	
-	void clearServiceState() {
-		// <write your code here>
-	}
-
+/*Cette ligne déclare une nouvelle classe publique nommée DefaultOrderManagementService. "Public" signifie que cette classe peut être accessible depuis d'autres parties du projet. La classe implémente l'interface OrderManagementService. Cela signifie que DefaultOrderManagementService doit fournir des implémentations pour toutes les méthodes définies dans l'interface.*/
+private static final int DEFAULT_ORDER_CAPACITY = 10;
+	/*Cette ligne déclare une constante privée statique de type entier nommée DEFAULT_ORDER_CAPACITY. La valeur est définie à 10. Cette constante représente probablement la taille par défaut allouée au stockage des commandes dans ce service.*/
+private static DefaultOrderManagementService instance;
+	/*Cette ligne déclare une variable statique privée nommée instance de type DefaultOrderManagementService. Cette variable contiendra probablement une seule instance de cette classe de service (modèle Singleton).*/
+// <write your code here>
+/*Ces commentaires indiquent des sections où vous devez écrire votre propre code pour implémenter les fonctionnalités.*/
+public static OrderManagementService getInstance() {
+    if (instance == null) {
+        instance = new DefaultOrderManagementService();
+    }
+    return instance;
 }
+	/*Cette méthode statique, nommée getInstance(), implémente le modèle Singleton. Elle vérifie si la variable instance est nulle (ce qui signifie qu'aucune instance n'a encore été créée). Si elle est nulle, elle crée une nouvelle instance de DefaultOrderManagementService et l'attribue à la variable instance. Enfin, elle renvoie l' instance. Cette approche garantit qu'il n'y a qu'une seule instance de ce service dans toute l'application.*/
+@Override
+public void addOrder(Order order) {
+    // <write your code here>
+}
+	/*Cette méthode est déclarée publique et redéfinit une méthode de l'interface OrderManagementService. Elle prend un objet Order en paramètre. Vous devez implémenter la logique ici pour ajouter une nouvelle commande au système. Cela pourrait impliquer le stockage de la commande dans une structure de données appropriée (par exemple, un tableau ou une liste) en gérant correctement les limitations de capacité.
+*/
+@Override
+public Order[] getOrdersByUserId(int userId) {
+    // <write your code here>
+    return null;
+}
+	/*Cette méthode redéfinit une autre méthode de l'interface. Elle prend un paramètre entier représentant un identifiant d'utilisateur. Vous devez implémenter la logique ici pour récupérer et renvoyer un tableau d'objets Order appartenant à l'utilisateur spécifié. Cela pourrait impliquer une recherche parmi les commandes stockées en fonction de l'identifiant d'utilisateur.*/
+@Override
+public Order[] getOrders() {
+    // <write your code here>
+    return null;
+}
+	/*Cette méthode redéfinit une méthode finale de l'interface. Elle ne prend aucun paramètre. Vous devez implémenter la logique ici pour récupérer et renvoyer un tableau d'objets Order représentant toutes les commandes du système. Cela pourrait*/
