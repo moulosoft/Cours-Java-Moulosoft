c3d07dfabcf64b56d58600205b8a0ceba28a112f
moulosoft <137833245+moulosoft@users.noreply.github.com>
2024-09-29 17:43:27 +0100

    Update OrderManagementService.java

--- src/com/itbulls/learnit/javacore/oop/exam/templates/onlineshop/services/OrderManagementService.java
+++ src/com/itbulls/learnit/javacore/oop/exam/templates/onlineshop/services/OrderManagementService.java
@@ -1,13 +1,19 @@
+// Ce package définit l'emplacement de ce fichier dans la structure du projet
 package com.itbulls.learnit.javacore.oop.exam.templates.onlineshop.services;
 
+// On importe la classe Order qui représente une commande
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.enteties.Order;
 
+// On définit une interface nommée OrderManagementService
+// Une interface en Java définit un contrat : elle spécifie les méthodes qu'une classe doit implémenter
 public interface OrderManagementService {
 
-	void addOrder(Order order);
+    // Cette méthode ajoute une nouvelle commande au système
+    void addOrder(Order order);
 
-	Order[] getOrdersByUserId(int userId);
-	
-	Order[] getOrders();
-
+    // Cette méthode retourne un tableau de commandes pour un utilisateur spécifique, identifié par son ID
+    Order[] getOrdersByUserId(int userId);
+    
+    // Cette méthode retourne un tableau de toutes les commandes du système
+    Order[] getOrders();
 }
