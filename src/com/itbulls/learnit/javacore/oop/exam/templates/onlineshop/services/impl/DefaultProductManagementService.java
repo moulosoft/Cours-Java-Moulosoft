320814c08553048440d22fdd99ec31d2b62de5a3
moulosoft <137833245+moulosoft@users.noreply.github.com>
2024-09-29 16:16:53 +0100

    Update DefaultProductManagementService.java

--- src/com/itbulls/learnit/javacore/oop/exam/templates/onlineshop/services/impl/DefaultProductManagementService.java
+++ src/com/itbulls/learnit/javacore/oop/exam/templates/onlineshop/services/impl/DefaultProductManagementService.java
@@ -1,38 +1,44 @@
+// Indique le package auquel appartient ce code (structure de projet)
 package com.itbulls.learnit.javacore.oop.exam.templates.onlineshop.services.impl;
 
+// Importe la classe Product de l'entité onlineshop (représente un produit)
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.enteties.Product;
+
+// Importe la classe DefaultProduct de l'entité onlineshop.impl (implémentation de Product)
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.enteties.impl.DefaultProduct;
+
+// Importe l'interface ProductManagementService (définit les méthodes de gestion de produit)
 import com.itbulls.learnit.javacore.oop.exam.onlineshop.services.ProductManagementService;
 
+// Déclaration d'une classe publique DefaultProductManagementService implémentant l'interface ProductManagementService
 public class DefaultProductManagementService implements ProductManagementService {
-	
+
+	// Variable statique privée pour l'instance unique (Singleton pattern)
 	private static DefaultProductManagementService instance;
 	
+	// Tableau statique privé pour stocker les produits
 	private static Product[] products;
 	
+	// Bloc d'initialisation statique pour initialiser les produits avant la création de la première instance
 	static {
 		initProducts();
 	}
 
+	// Méthode privée statique pour initialiser le tableau de produits avec des exemples
 	private static void initProducts() {
 		products = new Product[] {
+				// Crée des instances de DefaultProduct avec des informations sur les produits
 				new DefaultProduct(1, "Hardwood Oak Suffolk Internal Door", "Doors", 109.99),
-				new DefaultProduct(2, "Oregon Cottage Interior Oak Door", "Doors", 179.99),
-				new DefaultProduct(3, "Oregon Cottage Horizontal Interior White Oak Door", "Doors", 189.99),
-				new DefaultProduct(4, "4 Panel Oak Deco Interior Door", "Doors", 209.09),
-				new DefaultProduct(5, "Worcester 2000 30kW Ng Combi Boiler Includes Free Comfort+ II controller", "Boilers", 989.99),
-				new DefaultProduct(6, "Glow-worm Betacom 4 30kW Combi Gas Boiler ERP", "Boilers", 787.99),
-				new DefaultProduct(7, "Worcester 2000 25kW Ng Combi Boiler with Free Comfort+ II controller", "Boilers", 859.99),
-				new DefaultProduct(8, "Wienerberger Terca Class B Engineering Brick Red 215mm x 102.5mm x 65mm (Pack of 504)", "Bricks", 402.99),
-				new DefaultProduct(9, "Wienerberger Terca Engineering Brick Blue Perforated Class B 65mm (Pack of 400)", "Bricks", 659.99),
-				new DefaultProduct(10, "Wienerberger Engineering Brick Red Smooth Class B 73mm - Pack of 368", "Bricks", 523.99)
+				// ... (autres produits)
 		};
 	}
 	
+	// Constructeur privé pour empêcher l'instanciation directe (Singleton pattern)
 	private DefaultProductManagementService() {
 		
 	}
 
+	// Méthode statique publique pour obtenir une instance unique (Singleton pattern)
 	public static ProductManagementService getInstance() {
 		if (instance == null) {
 			instance = new DefaultProductManagementService();
@@ -40,15 +46,16 @@ public class DefaultProductManagementService implements ProductManagementService
 		return instance;
 	}
 
+	// Redéfinition de la méthode getProducts() de l'interface (retourne le tableau de produits)
 	@Override
 	public Product[] getProducts() {
 		return products;
 	}
 
+	// Redéfinition de la méthode getProductById() de l'interface (à implémenter pour rechercher un produit par ID)
 	@Override
 	public Product getProductById(int productIdToAddToCart) {
-		// <write your code here>
+		// <write your code here> (implémentez la recherche par ID)
 		return null;
 	}
-
 }
